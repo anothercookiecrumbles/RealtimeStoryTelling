5baeaa0c171641236c0f23c44df92674e6797a86
anothercookiecrumbles <priyanjana@mac.com>
2016-02-10 01:56:20 -0500

    - Adds comments.

--- TwitterStreamer/client/map.js
+++ TwitterStreamer/client/map.js
@@ -1,8 +1,11 @@
 <!-- Creates the initial world map. --> 
 
+/** Needs to be accessed by the WebSocket layer to ensure that the circles are projected correctly. 
+Hence, this is a global variable. */
 var projection; 
 
 function drawMap() {
+  /** Specifies the dimensions in which the map will be drawn. */
   var width = 970,
   height = 800;
 
@@ -10,18 +13,36 @@ function drawMap() {
     .attr("width", width)
     .attr("height", height);
 
+  /** Load the world.json file. This file was generated using: Natural Earth's 1:10m map, which 
+      contains the country's polygons (http://www.naturalearthdata.com/http//www.naturalearthdata
+      .com/download/10m/cultural/ne_10m_admin_0_map_subunits.zip)
+
+      In order to generate the TopoJSON, I followed this tutorial: 
+      https://bost.ocks.org/mike/map/. 
+  */
   d3.json("world.json", function(error, world) {
     var path = d3.geo.path();
+    // Pull out the country polygons from the (topo)json file. 
     var featureCollection = topojson.feature(world, world.objects.places);
+    // ...and creates the bounds for these "features." 
+    // @see https://github.com/mbostock/d3/wiki/Geo-Paths#bounds
     var bounds = d3.geo.bounds(featureCollection);
 
+    // Necessary to center the map correctly based on the latitude/longitude returned. 
     var centerX = d3.sum(bounds, function(d) {return d[0];}) / 2
     var centerY = d3.sum(bounds, function(d) {return d[1];}) / 2
 
+    console.log(centerX)
+    console.log(centerY)
+
     projection = d3.geo.mercator()
       .scale(130)
-      .center([centerX, centerY+75]); // Added offset to Y-axis to ensure it looks properly centered. 
+      /* Added offset to Y-axis to ensure it looks properly centered. Essentially, this means 
+         that Antarctica's dropped off the map. I _think_ this is acceptable considering no one's 
+         tweeting from Antarctica, but...? */ 
+      .center([centerX, centerY+75]);  
 
+    // Add the projection to the path, and finally, draw the map. 
     path.projection(projection);
     svg.selectAll("path")
       .data(featureCollection.features)
--- TwitterStreamer/client/websocket.js
+++ TwitterStreamer/client/websocket.js
@@ -1,9 +1,14 @@
 var ws = new WebSocket("ws://localhost:8080")
 
+/**
+  * Invoked when we receive a message over websocket from the server. 
+  * @param: event: The MessageEvent received from the server that contains the data that we are 
+      interested in. 
+*/
 ws.onmessage = function(event) {
   data = JSON.parse(event.data)
   console.log(data)
-  var hashtag = data["hashtag"].toLowerCase()
+  var hashtag = data["hashtag"].toLowerCase() // easier for string comparisons 
   var coordinates = data["coordinates"]
 
   // The colour codes are based on the logo colours of each of the organisations. 
@@ -20,14 +25,18 @@ ws.onmessage = function(event) {
     color = '#35a953' // As the other colours of the Google/Alphabet logo are taken, using green. 
   }
 
+  /* Finds the map in d3 and projects a circle of radius 4 on the map, which is displayed and then 
+  fades out five seconds later. */ 
   var group = d3.select("svg").append("g")
   group.append('circle')
-    .attr('r', 4)
-    .attr("transform", function(d) {return "translate(" + projection(coordinates)[0] + ',' + projection(coordinates)[1] + ")";})
-    .style('fill', color)
-    .style('opacity', 1)
+    .attr('r', 4) // radius of the circle 
+    // ensure that the circle is projected correctly based on how the map was initially constructed. 
+    .attr("transform", function(d) {return "translate(" + projection(coordinates)[0] + ',' + 
+      projection(coordinates)[1] + ")";}) 
+    .style('fill', color) 
+    .style('opacity', 1) 
     .transition()
-    .duration(5000)
+    .duration(5000) 
     // To keep the dots in place, you could, in theory, comment out/remove this line. 
     .style("opacity", 0); 
   }
