56c5d6fd98395ed108135259f36ece14897b9c1a
anothercookiecrumbles <priyanjana@mac.com>
2016-02-10 02:15:12 -0500

    Uses absolute path to find the configuration because relative paths are evil.

--- TwitterStreamer/server/twitter-subscriber/src/Bootstrap.py
+++ TwitterStreamer/server/twitter-subscriber/src/Bootstrap.py
@@ -1,6 +1,7 @@
 from TwitterSubscriber import TwitterSubscriber
 from TwitterListener import TwitterListener
 from WebSocketSender import WebSocketSender
+import os
 
 """As the name suggests, the bootstrapper."""
 
@@ -15,27 +16,30 @@ case-insensitive."""
 HASHTAGS=['#facebook', '#amazon', '#netflix', '#google', '#alphabet', '#apple']
 
 def main():
-  """Initialises the WebSocketSender, which creates the WebSocket connection and streams data down."""
-  sender = WebSocketSender()
-
-  """Initialises the TwitterListener, which has a reference to the WebSocketSender, simply to be able to _send_
-  messages down to the client(s)."""
-  listener = TwitterListener(sender, HASHTAGS)
-
   """Pulls out OAuth keys from the configuration file, which is needed to authenticate with Twitter.
   TODO: Should really look into making this more elegant. For example, if any of the keys or values had an "=",
   this wouldn't work as expected. For now, this isn't the case, so something to come back to."""
   oauthKeys = {}
   try:
-    with open(CONFIG_FILE) as config:
+    #Handle relative and absolute paths with equal grace.
+    current = os.path.dirname(os.path.abspath(__file__)) + "/x"
+    absoluteConfigFile = current + CONFIG_FILE
+    with open(absoluteConfigFile) as config:
       for line in config:
         if not line.strip() == "":
           (key, value) = line.split("=")
           oauthKeys[key.strip()] = value.strip()
   except FileNotFoundError as ex:
     print("Unable to find the configuration {}. Please ensure that it exists in the correct location."
-        .format(CONFIG_FILE))
-    raise FileNotFoundError("No file exists: {}".format(CONFIG_FILE))
+        .format(absoluteConfigFile))
+    raise FileNotFoundError("No file exists: {}".format(absoluteConfigFile))
+
+  """Initialises the WebSocketSender, which creates the WebSocket connection and streams data down."""
+  sender = WebSocketSender()
+
+  """Initialises the TwitterListener, which has a reference to the WebSocketSender, simply to be able to _send_
+  messages down to the client(s)."""
+  listener = TwitterListener(sender, HASHTAGS)
 
   """Create the TwitterSubscriber object, which in turn initializes Tweepy, which in turn authorises the connection
   with Twitter."""
